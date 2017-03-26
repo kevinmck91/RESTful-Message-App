7ebb628cf560660043d4b0ae14ab88e91942e483
kevinmck91 <kevmckeon@gmail.com>
2017-03-26 11:53:58 +0100

    Hard code messages and return them in an XML request from database

--- src/main/java/com/kevin/messenger/service/MessageService.java
+++ src/main/java/com/kevin/messenger/service/MessageService.java
@@ -2,22 +2,55 @@ package com.kevin.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import com.kevin.messenger.database.DatabaseClass;
 import com.kevin.messenger.model.Message;
 
-	//set of operations on the a message(s)
-
+//set of operations on the a message(s)
 public class MessageService {
 	
-	// hardcoded a list of created messages
+
+	//access the messages in database
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
 	
-	public List<Message> getAllMessages(){
-		Message m1 = new Message(1L, "Hello World!", "Kevin" );
-		Message m2 = new Message(2L, "Hello Jersey!", "Kevin" );
-		List<Message> list = new ArrayList<>();
-		list.add(m1);
-		list.add(m2);
-		return list;
+	//Add new hardcoded message to the database
+	public MessageService(){
+		messages.put(1L, new Message(1, "Hello World", "kevin"));
+		messages.put(2L, new Message(2, "Hello Jersey", "kevin"));
 	}
+	
+	
+	//Returns all the messages from the database
+	public List<Message> getAllMessages(){
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	//Returns a single method
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+	
+	//Adds a message to the hash table / database
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message) {
+		if (message.getId() <= 0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	//Delete a message
+	public Message removeMessage(long id) {
+		return messages.remove(id);
+	}
+	
+
 
 }
