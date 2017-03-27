caa2d6cc5ff7dc941fcb79c28c120ff6a5d991d5
kevinmck91 <kevmckeon@gmail.com>
2017-03-27 21:41:27 +0100

    add method to update and delete messages from the database

--- src/main/java/com/kevin/messenger/resources/MessageResource.java
+++ src/main/java/com/kevin/messenger/resources/MessageResource.java
@@ -2,7 +2,11 @@ package com.kevin.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -44,5 +48,28 @@ public class MessageResource {
 		
 	}
 	
+	//Allows you to send JSON in the POST and
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)	
+	public Message addMessage(Message message){
+		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)	
+	public Message updateMessage(@PathParam("messageId") long id, Message message){
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)	
+	public void deleteMessage(@PathParam("messageId") long id){
+		messageService.removeMessage(id);
+	}
 	
 }
